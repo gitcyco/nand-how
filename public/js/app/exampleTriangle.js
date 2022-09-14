be91c49430261a329c609df168f319f5c316163c
gitcyco <gitcyco@users.noreply.github.com>
2022-09-14 14:55:48 -0700

    start layout

--- public/js/app/exampleTriangle.js
+++ public/js/app/exampleTriangle.js
@@ -1,10 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var canvas = new draw2d.Canvas("gfx_holder");
+  const canvas = new draw2d.Canvas("main-canvas");
 
-  var triangle = new TriangleFigure({ x: 100, y: 100, width: 100, height: 140 });
+  // Add a simple triangle
+  let triangle = new TriangleFigure({ x: 100, y: 100, width: 100, height: 140 });
   canvas.add(triangle);
 
-  // JUST ADD SOME DOCU ELEMENTS ON THE SCREEN
-  var msg = new draw2d.shape.note.PostIt({ text: "Simple example how to create your own vector figure" });
+  // Add a basic PostIt note
+  let msg = new draw2d.shape.note.PostIt({ text: "Just a basic triangle" });
   canvas.add(msg, 20, 20);
 });
