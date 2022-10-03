585e962f582da8cfb26c863959c50a67a2f14263
gitcyco <gitcyco@users.noreply.github.com>
2022-10-02 17:14:48 -0700

    add svg's for icons

--- public/js/figures/AND_clean.js
+++ public/js/figures/AND_clean.js
@@ -1,10 +1,3 @@
-// Generated Code for the Draw2D touch HTML5 lib
-//
-// http://www.draw2d.org
-//
-// Go to the Designer http://www.draw2d.org
-// to design your own shape or download user generated
-//
 let AndClean = draw2d.SetFigure.extend({
   NAME: "AndClean",
 
